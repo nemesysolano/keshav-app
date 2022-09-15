92c7dfcfc213810d243e3ba9004a03e8bf556b40
rsolano <nemesysolano@gmail.com>
2022-09-15 01:03:16 -0400

    Added function to fetch token

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TwilioSession, useTwilioSession } from './hooks';
-import { Conversation } from '@twilio/conversations'
+import { Client, Conversation, Message, Participant } from '@twilio/conversations'
 import axios from 'axios';
 
+const TWILIO_URL='https://api.9PackSoftware.com'
+const SAMPLE_USER_NAME = "124"; 
 
-const ConversationListing: React.FC<{conversations: Conversation[]}> = ({conversations}) => {
+interface ConversationListingData {
+  conversation: Conversation;
+  participants: Participant[];
+  messages: Message[];
+}
+
+const ConversationListing: React.FC<{conversationListingData: ConversationListingData[]}> = ({conversationListingData}) => {
+  /* 
+  friendlyName -> Card title ("Dispatcher Chat")
+  participants.length -> Small avatars, draw as many as this value indicates
+  lastMessage -> last message in chat, it may be '' if no message has been sent.
+  */
+
   return (
     <ul>
-      {conversations.map((conversation: Conversation, index: number) => (<li key={index}><>{conversation.friendlyName}</></li>))}
+      {conversationListingData.map((conversationListingData: ConversationListingData, index: number) => 
+        {
+          const {conversation, participants, messages} = conversationListingData;
+          const lastMessage = messages.length === 0 ? "" : messages[0].body;
+          const dateCreated = conversation.dateCreated || new Date();
+          return (
+            <li key={index}>
+              <>
+                {conversation.friendlyName} contains {participants.length} participants and last message is '{lastMessage}'. Created on {dateCreated.toLocaleString()}                
+              </>
+            </li>            
+          )
+        })
+      }
     </ul>
   )
 }
 
+
+const getTwilioToken = async (user: string): Promise<string> => {
+  const url = `${TWILIO_URL}/communication/auth`;
+
+  const response = await axios.post(
+    url, 
+    {
+      serviceSid: 'ISc3df9f655e83415e9bf7ef48599575bd',
+      identity: user,
+    }
+  );
+  
+  return response.data.content.token; 
+};
+
 const subscribedConversations = async (
-  twilioSession: TwilioSession
+  client: Client,
 ): Promise<Conversation[]> => {
   
-  const client = twilioSession.client;
   let subscribedConversations = await client.getSubscribedConversations();
   let conversations = subscribedConversations.items;
 
@@ -30,20 +70,53 @@ const subscribedConversations = async (
   return conversations;
 }
 
-export const App: React.FC = () => {
-  const twilioSession = useTwilioSession();
-  const [conversations, setConversations] = useState<Conversation[]>([]);
-
+export const App: React.FC = () => {  
+  const [conversationListingData, setConversationListingData] = useState<ConversationListingData[]>([]);
+  
   useEffect(() => {
-    (async () => {
-      const conversations = await subscribedConversations(twilioSession)
-      setConversations(conversations);
+    (async () => {      
+      /* 
+      * Fetch the token after user logs in and keep it in the global state.
+      */
+      const twilioToken = await getTwilioToken(SAMPLE_USER_NAME); 
+
+      /**
+       * Create the twilio after user logs and keep it in the global state.
+       */
+      const client = new Client(twilioToken);
+      const conversations = await subscribedConversations(client)      
+      const conversationListingData: ConversationListingData[] = [];
+
+      for(let index = 0; index < conversations.length; index++){
+          const conversation = conversations[index];
+
+          /* Fetch participants */
+          const participants = await conversation.getParticipants();
+          
+          /* Fetch messages */
+          const messageCount = await conversation.getMessagesCount();
+          let messages: Message[] = [];
+
+          if(messageCount > 1) {
+            const messageReader = await conversation.getMessages(1, 0);            
+            const page = await messageReader.nextPage(); 
+            messages = [...page.items];
+          }
+          conversationListingData.push({
+            conversation,
+            participants,
+            messages
+          })
+
+      }
+      
+      setConversationListingData(conversationListingData);      
     })();
   },[]);
 
   return (
     <div className="App">
-      <ConversationListing conversations={conversations} />
+      <ConversationListing conversationListingData={conversationListingData} />
     </div>
   );
 }
